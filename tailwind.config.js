0d2fab2cf39ccd035acaa38b362c49d87e7197c6
sujeetkumar07 <sujeet.kumar.aot07@gmail.com>
2025-06-03 13:36:32 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")], // ✅ Proper place for DaisyUI
+}
