ea06d723c905f9daf8daf2cc65bec0951dd4696d
sujeetkumar07 <sujeet.kumar.aot07@gmail.com>
2025-06-09 20:26:19 +0530

    all API implementation with signupPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,57 +2,34 @@ import { useState } from "react";
 import "./App.css";
 import "./index.css";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./component/Body";
+import Login from "./component/Login";
+import Profile from "./component/Profile";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
+import Feed from "./component/Feed";
+import Connections from "./component/Connections";
+import Requests from "./component/Requests";
+
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1 class="text-xs font-bold underline">Hello Baby</h1>
-      <button className="btn btn-success">Daisy UI Button</button>
-      <div className="navbar bg-base-100 shadow-sm">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div className="flex gap-2">
-          <input
-            type="text"
-            placeholder="Search"
-            className="input input-bordered w-24 md:w-auto"
-          />
-          <div className="dropdown dropdown-end">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                />
-              </div>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </>
+    <Provider store={appStore}>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/" element={<Feed />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/connections" element={<Connections />} />
+             <Route path="/requests" element={<Requests />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
